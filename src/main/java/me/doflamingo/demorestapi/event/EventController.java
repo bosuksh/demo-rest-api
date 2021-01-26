db13971d7c81836c3e52e8bca868e01cb6ed656e
Lee SangHyup <bosuksh@gmail.com>
2021-01-27 01:13:23 +0900

    feat: Add EventRepository
    EventRepository를 통해서 실제 save 수행

--- src/main/java/me/doflamingo/demorestapi/event/EventController.java
+++ src/main/java/me/doflamingo/demorestapi/event/EventController.java
@@ -1,5 +1,6 @@
 package me.doflamingo.demorestapi.event;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,13 +14,16 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @RestController
 @RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
+@RequiredArgsConstructor
 public class EventController {
 
+  private final EventRepository eventRepository;
+
   @PostMapping
-  public ResponseEntity createEvents(@RequestBody Event event) {
-    event.setId(100);
-    URI uri = linkTo(EventController.class).slash("{id}").toUri();
-    return ResponseEntity.created(uri).body(event);
+  public ResponseEntity<?> createEvents(@RequestBody Event event) {
+    Event newEvent = eventRepository.save(event);
+    URI uri = linkTo(EventController.class).slash(newEvent).toUri();
+    return ResponseEntity.created(uri).body(newEvent);
   }
 
 }
--- src/test/java/me/doflamingo/demorestapi/event/EventControllerTest.java
+++ src/test/java/me/doflamingo/demorestapi/event/EventControllerTest.java
@@ -4,16 +4,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest
 class EventControllerTest {
@@ -24,6 +26,10 @@ class EventControllerTest {
   @Autowired
   ObjectMapper objectMapper;
 
+  @MockBean
+  EventRepository eventRepository;
+
+
   @Test
   public void createEvent() throws Exception {
     //given
@@ -31,24 +37,28 @@ class EventControllerTest {
                     .name("Spring")
                     .description("Spring Study")
                     .beginEventDateTime(LocalDateTime.of(2021,1,27,10,0))
-                    .endEventDateTime(LocalDateTime.of(2021,1,27,12,00))
-                    .beginEnrollmentDateTime(LocalDateTime.of(2021,1,10,10,00))
-                    .closeEnrollmentDateTime(LocalDateTime.of(2021,1,20,10,00))
+                    .endEventDateTime(LocalDateTime.of(2021,1,27,12,0))
+                    .beginEnrollmentDateTime(LocalDateTime.of(2021,1,10,10,0))
+                    .closeEnrollmentDateTime(LocalDateTime.of(2021,1,20,10,0))
                     .basePrice(100)
                     .maxPrice(300)
                     .limitOfEnrollment(100)
                     .eventStatus(EventStatus.DRAFT)
                     .location("강남역 D2 스타트업 팩토리")
                     .build();
+    event.setId(100);
+    when(eventRepository.save(event)).thenReturn(event);
     //when
     mockMvc.perform(post("/api/events")
       .contentType(MediaType.APPLICATION_JSON)
-      .accept(MediaTypes.HAL_JSON)
+      .accept(MediaTypes.HAL_JSON+";charset=UTF-8")
       .content(objectMapper.writeValueAsString(event))
     )
     //then
     .andDo(print())
     .andExpect(status().isCreated())
+    .andExpect(header().exists(HttpHeaders.LOCATION))
+    .andExpect(header().string(HttpHeaders.CONTENT_TYPE,MediaTypes.HAL_JSON_VALUE+";charset=UTF-8"))
     .andExpect(jsonPath("id").exists())
     ;
 
