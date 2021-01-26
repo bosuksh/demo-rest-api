6e96d39ac1d522b30f0bbb7dfe4e91c636949ca6
Lee SangHyup <bosuksh@gmail.com>
2021-01-27 00:42:25 +0900

    feat: Add EventController
    테스트케이스 통과

--- src/main/java/me/doflamingo/demorestapi/event/EventController.java
+++ src/main/java/me/doflamingo/demorestapi/event/EventController.java
@@ -1,4 +1,25 @@
 package me.doflamingo.demorestapi.event;
 
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+@RestController
+@RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
 public class EventController {
+
+  @PostMapping
+  public ResponseEntity createEvents(@RequestBody Event event) {
+    event.setId(100);
+    URI uri = linkTo(EventController.class).slash("{id}").toUri();
+    return ResponseEntity.created(uri).body(event);
+  }
+
 }
--- src/test/java/me/doflamingo/demorestapi/event/EventControllerTest.java
+++ src/test/java/me/doflamingo/demorestapi/event/EventControllerTest.java
@@ -1,5 +1,6 @@
 package me.doflamingo.demorestapi.event;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -7,7 +8,11 @@ import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
@@ -16,11 +21,35 @@ class EventControllerTest {
   @Autowired
   MockMvc mockMvc;
 
+  @Autowired
+  ObjectMapper objectMapper;
+
   @Test
   public void createEvent() throws Exception {
+    //given
+    Event event = Event.builder()
+                    .name("Spring")
+                    .description("Spring Study")
+                    .beginEventDateTime(LocalDateTime.of(2021,1,27,10,0))
+                    .endEventDateTime(LocalDateTime.of(2021,1,27,12,00))
+                    .beginEnrollmentDateTime(LocalDateTime.of(2021,1,10,10,00))
+                    .closeEnrollmentDateTime(LocalDateTime.of(2021,1,20,10,00))
+                    .basePrice(100)
+                    .maxPrice(300)
+                    .limitOfEnrollment(100)
+                    .eventStatus(EventStatus.DRAFT)
+                    .build();
+    //when
     mockMvc.perform(post("/api/events")
-        .contentType(MediaType.APPLICATION_JSON)
-        .accept(MediaTypes.HAL_JSON))
-      .andExpect(status().isCreated());
+      .contentType(MediaType.APPLICATION_JSON)
+      .accept(MediaTypes.HAL_JSON)
+      .content(objectMapper.writeValueAsString(event))
+    )
+    //then
+    .andDo(print())
+    .andExpect(status().isCreated())
+    .andExpect(jsonPath("id").exists())
+    ;
+
   }
 }
