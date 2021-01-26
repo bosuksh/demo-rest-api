7dd7ee7493acbc3e5d69cc4145799b532c91a84f
Lee SangHyup <bosuksh@gmail.com>
2021-01-26 23:48:10 +0900

    feat: Add EventDomain and TestCode 도메인 클래스와 테스트 그걸 확인하는 자바빈 및 빌더 테스트코드 추가

---
+++ src/main/java/me/doflamingo/demorestapi/event/EventStatus.java
@@ -0,0 +1,5 @@
+package me.doflamingo.demorestapi.event;
+
+public enum EventStatus {
+  DRAFT, PUBLISHED, BEGAN_ENROLLMENT, CLOSE_ENROLLMENT, STARTED, END
+}
---
+++ src/test/java/me/doflamingo/demorestapi/event/EventTest.java
@@ -0,0 +1,30 @@
+package me.doflamingo.demorestapi.event;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EventTest {
+
+  @Test
+  public void builder() {
+    Event event = Event.builder().build();
+    assertThat(event).isNotNull();
+  }
+
+
+  @Test
+  public void javaBean() {
+    //given
+    Event event = new Event();
+    String name = "Spring";
+    String description = "SpringBoot Rest API";
+    //when
+    event.setName(name);
+    event.setDescription(description);
+    //then
+    assertThat(event.getName()).isEqualTo(name);
+    assertThat(event.getDescription()).isEqualTo(description);
+  }
+
+}
