96ef96b68974728124e64dd1155bbc212707aa74
Lee SangHyup <bosuksh@gmail.com>
2021-02-06 22:12:49 +0900

    test: 인증 실패 테스트 추가

--- src/test/java/me/doflamingo/demorestapi/accounts/service/AccountServiceTest.java
+++ src/test/java/me/doflamingo/demorestapi/accounts/service/AccountServiceTest.java
@@ -8,15 +8,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class AccountServiceTest {
+
   @Autowired
   AccountService accountService;
   @Autowired
@@ -37,5 +40,16 @@ class AccountServiceTest {
     UserDetails userDetails = accountService.loadUserByUsername(account.getEmail());
     //then
     assertThat(userDetails.getPassword()).isEqualTo(password);
+  }
+
+  @Test
+  @DisplayName("인증 실패 과정")
+  public void findByEmailFail() {
+    //given
+    String randomName = "random@exmaple.com";
+    //when
+    UsernameNotFoundException usernameNotFoundException = assertThrows(UsernameNotFoundException.class, () -> accountService.loadUserByUsername(randomName));
+    //then
+    assertThat(usernameNotFoundException).hasMessageContaining(randomName);
   }
 }
