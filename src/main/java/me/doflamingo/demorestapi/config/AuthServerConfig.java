4fbd06e10df54a27424091930a257305ae2c35f9
Lee SangHyup <bosuksh@gmail.com>
2021-02-07 00:33:22 +0900

    test: AuthServer Test

---
+++ src/main/java/me/doflamingo/demorestapi/config/AuthServerConfig.java
@@ -0,0 +1,4 @@
+package me.doflamingo.demorestapi.config;
+
+public class AuthServerConfig {
+}
---
+++ src/test/java/me/doflamingo/demorestapi/config/AuthServerConfigTest.java
@@ -0,0 +1,49 @@
+package me.doflamingo.demorestapi.config;
+
+import me.doflamingo.demorestapi.accounts.domain.Account;
+import me.doflamingo.demorestapi.accounts.domain.AccountRole;
+import me.doflamingo.demorestapi.accounts.service.AccountService;
+import me.doflamingo.demorestapi.common.BaseControllerTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class AuthServerConfigTest extends BaseControllerTest {
+
+  @Autowired
+  AccountService accountService;
+
+  @Test
+  @DisplayName("인증 토큰 받기")
+  public void getAuthToken() throws Exception {
+    //given
+    String email = "doflamingo@example.com";
+    String password = "password";
+    Account.builder()
+      .email(email)
+      .password(password)
+      .accountRoles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+      .build();
+    String clientId = "myApp";
+    String clientSecret = "pass";
+    //when
+    this.mockMvc.perform(post("/oauth/token")
+                           .with(httpBasic(clientId, clientSecret))
+      .param("grant_type","password")
+      .param("email", email)
+      .param("password", password)
+    )
+      .andDo(print())
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("access_token").exists());
+    //then
+  }
+}
