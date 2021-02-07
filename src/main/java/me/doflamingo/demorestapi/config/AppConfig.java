b052cbfb7f92e56b460fa8a343afea7904027cc2
Lee SangHyup <bosuksh@gmail.com>
2021-02-07 11:04:53 +0900

    feat: applicationRunner를 통해 유저 두명 미리 추가

--- src/main/java/me/doflamingo/demorestapi/config/AppConfig.java
+++ src/main/java/me/doflamingo/demorestapi/config/AppConfig.java
@@ -1,10 +1,18 @@
 package me.doflamingo.demorestapi.config;
 
+import me.doflamingo.demorestapi.accounts.domain.Account;
+import me.doflamingo.demorestapi.accounts.domain.AccountRole;
+import me.doflamingo.demorestapi.accounts.service.AccountService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
 
 @Configuration
 public class AppConfig {
@@ -17,5 +25,30 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public ApplicationRunner appRunner() {
+        return new ApplicationRunner() {
+            @Autowired
+            AccountService accountService;
+
+
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                Account admin= Account.builder()
+                                    .email("admin@example.com")
+                                    .password("admin")
+                                    .accountRoles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                                    .build();
+                Account user= Account.builder()
+                                 .email("user@example.com")
+                                 .password("user")
+                                 .accountRoles(Set.of(AccountRole.USER))
+                                 .build();
+                accountService.saveAccount(admin);
+                accountService.saveAccount(user);
+            }
+        };
     }
 }
